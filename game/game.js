8ff0e9a4cdf70759273eab5d0a179c01647e8082
xiranou <xiranou@gmail.com>
2015-01-10 17:25:55 -0500

    update game.js to have State object, can add other object's state functions to it

--- game/game.js
+++ game/game.js
@@ -1,1 +1,13 @@
-var game = game || new Phaser.Game(800,600,Phaser.AUTO,'ryuman',state);
+var State = {};
+
+State.preload = function(){
+  Ryu.preload();
+};
+State.create = function(){
+  Ryu.create();
+};
+State.update = function(){
+  Ryu.update();
+};
+
+var game = game || new Phaser.Game(800,600,Phaser.AUTO,'ryuman',State);
