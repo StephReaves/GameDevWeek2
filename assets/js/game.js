3b54ea74f0cadd507c7168e52c238d1de158bfd5
Michael Angelo <yamikamisama@gmail.com>
2015-01-10 15:51:35 -0500

    megaman image flips with movement

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,14 +11,17 @@ function create() {
     playerSprite = game.add.sprite(0, 600, 'megaman');
     game.physics.enable(playerSprite, Phaser.Physics.ARCADE);
     playerSprite.body.collideWorldBounds = true;
+    playerSprite.body.drag.x = 300;
 
 }
 
 function update() {
 	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
 		playerSprite.body.velocity.x = -200;
+		playerSprite.scale.x = -1;
 	} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
 		playerSprite.body.velocity.x = 200;
+		playerSprite.scale.x = 1;
 	}
 
 };
