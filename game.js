556c2ad4170062ba8fcb33470dbe61c29b0652a3
Michael Angelo <yamikamisama@gmail.com>
2015-01-10 15:20:53 -0500

    boundaries are set

--- game.js
+++ game.js
@@ -8,18 +8,16 @@ function preload() {
 function create() {
 
     playerSprite = game.add.sprite(0, 0, 'megaman');
-
     game.physics.enable(playerSprite, Phaser.Physics.ARCADE);
-
-    // playerSprite.body.velocity.x=150;
+    playerSprite.body.collideWorldBounds = true;
 
 }
 
 function update() {
 	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-		playerSprite.body.velocity.x = -50;
+		playerSprite.body.velocity.x = -200;
 	} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-		playerSprite.body.velocity.x = 50;
+		playerSprite.body.velocity.x = 200;
 	}
 
 };
