9ef07cb315a6b5ad42f071b21a5843fa5e14e920
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-01-11 13:08:07 -0500

    add sprite to game

--- game/sprite_movement.js
+++ game/sprite_movement.js
@@ -1,12 +1,13 @@
 var RyuMove = {};
 
 RyuMove.preload = function() {
-	game.load.spritesheet('ryuMove', 'assets/ryu/ryu_ken_spritesheet.png');
+	game.load.spritesheet('ryuMove', 'assets/ryu/ryu_ken_spritesheet.png', 32, 48);
 
 }
 
 RyuMove.create = function() {
-
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	ryuMove = game.add.sprite(50, 50, 'ryuMove');
 }
 
 RyuMove.update = function() {
