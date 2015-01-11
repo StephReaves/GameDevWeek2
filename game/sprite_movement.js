e73531cb703fb4fb212e7ddc1ce3f2fc068e8149
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-01-11 13:37:18 -0500

    add keys for movement

--- game/sprite_movement.js
+++ game/sprite_movement.js
@@ -9,11 +9,21 @@ RyuMove.preload = function() {
 RyuMove.create = function() {
 	ryuMove = game.add.sprite(32, game.world.height - 150, 'ryuMove');
 	game.physics.arcade.enable(ryuMove);
-	ryuMove.body.bounce.y = 0.2;
-	ryuMove.body.gravity.y = 200;
+	// ryuMove.body.bounce.y = 0.2;
+	// ryuMove.body.gravity.y = 200;
 	ryuMove.body.collideWorldBounds = true;
 
 	ryuMove.animations.add('right', [1, 2, 3, 4], 10, true); 
+
+	playerKeys = {
+    //movement
+    w: game.input.keyboard.addKey(87),
+    a: game.input.keyboard.addKey(65),
+    s: game.input.keyboard.addKey(83),
+    d: game.input.keyboard.addKey(68),
+    //hadoken
+    // f: game.input.keyboard.addKey(70)
+  };
 
 }
 
