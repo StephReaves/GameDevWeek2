f41a7bbbaac4fa6d2741fb32152dce13db1a6565
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-01-11 13:04:51 -0500

    define create, update, preload methods, also load spritesheet

---
+++ game/sprite_movement.js
@@ -0,0 +1,14 @@
+var RyuMove = {};
+
+RyuMove.preload = function() {
+	game.load.spritesheet('ryuMove', 'assets/ryu/ryu_ken_spritesheet.png');
+
+}
+
+RyuMove.create = function() {
+
+}
+
+RyuMove.update = function() {
+
+}
