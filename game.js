baa257734f8df66b50eb38ddc8450224f2b83470
Michael Angelo <yamikamisama@gmail.com>
2015-01-11 15:02:10 -0500

    merge fix

--- game.js
+++ game.js
@@ -14,9 +14,16 @@ function create() {
 	playerSprite = game.add.sprite(game.world.centerX, 0, 'megaman');
 	playerSprite.acceleration.y = 200;
 	playerSprite.acceleration.x = 100;
+  playerSprite = game.add.sprite(0, 0, 'megaman');
+  game.physics.enable(playerSprite, Phaser.Physics.ARCADE);
+  playerSprite.body.collideWorldBounds = true;
 
 };
 
 function update() {
-
+	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+		playerSprite.body.velocity.x = -200;
+	} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+		playerSprite.body.velocity.x = 200;
+	}
 };
