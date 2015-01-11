467c7189eb3535249da5e02d53841005d2127b15
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-01-11 13:47:35 -0500

    add conditional for key strokes, not working yet

--- game/sprite_movement.js
+++ game/sprite_movement.js
@@ -16,10 +16,21 @@ RyuMove.create = function() {
 	ryuMove.animations.add('right', [1, 2, 3, 4], 10, true); 
 
 	cursors = game.input.keyboard.createCursorKeys();
-	
+
 
 }
 
 RyuMove.update = function() {
+	ryuMove.body.velocity.x = 0;
 	
+	if (cursors.right.isDown) {
+		ryuMove.body.velocity.x = 150;
+		ryuMove.animations.play('right');
+	} else {
+		ryuMove.animations.stop();
+		ryuMove.frame = 4;
+	}
+	if (cursors.up.isDown && ryuMove.body.touching.down) {
+		ryuMove.body.velocity.y	= -250;
+	}
 }
