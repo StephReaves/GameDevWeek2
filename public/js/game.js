a9d780ab081e8c7ada5ec38cba24dfb16d76385f
xiranou <xiranou@gmail.com>
2015-01-14 16:25:54 -0500

    add level_one_redo sample json

--- public/js/game.js
+++ public/js/game.js
@@ -2,27 +2,20 @@ var State = {};
 var music;
 
 State.preload = function(){
-  game.load.tilemap('level1', 'assets/levels/level_one.json', null, Phaser.Tilemap.TILED_JSON);
-  game.load.image('background', 'assets/imgs/background_level1.png');
-  game.load.atlasJSONHash('ryuRun', 'assets/ryu/ryu.json', 'assets/ryu/ryu.png');
+  game.load.tilemap('level_one', 'assets/levels/level_one_redo.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.image('level_one_tiles', 'assets/imgs/background_level1.png');
+  // game.load.atlasJSONHash('ryuRun', 'assets/ryu/ryu.json', 'assets/ryu/ryu.png');
   Ryu.preload();
 
 };
 State.create = function(){
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  game.add.tileSprite(0, 0, 4060, 224, 'background');
-  game.world.setBounds(0, 0, 4060, 224);
+  map = game.add.tilemap('level_one');
+  map.addTilesetImage('level_one_tiles');
+  background = map.createLayer('Tile Layer 1');
+  background.resizeWorld();
+
   Ryu.create();
-  map = game.add.tilemap('level1');
-  map.addTilesetImage('background_level1', 'background');
-  map.setCollisionBetween(1, 100000, true, 'Collision');
-  // 'ryuman_level1' is the name used in Tiled Map Editor
-  // 'Background' is the name of the layer in the tiled file
-  // this.backgroundLayer = this.map.createLayer('Background');
-  // this.blockedLayer = this.createLayer('Collision');
-  //collision on blockedLayer
-  //resizes the game world
-  //this.backgroundLayer.resizeWorld();
 
 };
 State.update = function(){
