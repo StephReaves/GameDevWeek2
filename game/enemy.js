78ac5cdaa25c46e9628e4860ddf6353eeacd8f3c
jhwang90 <jhwang90@gmail.com>
2015-01-14 00:00:03 -0800

    Modify ryu and enemy to use phaser health instead

--- game/enemy.js
+++ game/enemy.js
@@ -1,35 +1,33 @@
 var Enemy = {};
 
-var enemyHealth = 2;
-
 Enemy.preload = function() {
 	game.load.image('enemy', 'assets/enemy/enemy.png');
 };
 
 Enemy.create = function() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-	enemy = game.add.image(game.world.centerX, 320,'enemy');
+	enemy = game.add.image(120, 520,'enemy');
 	game.physics.arcade.enable(enemy);
 	// Enemy physical attributes
+	enemy.health = 2
 	enemy.anchor.setTo(.5,.5);
 	enemy.animations.add('left',[0,1],10,true);
 	enemy.animations.add('right',[1,2],10,true);
 };
 
 Enemy.update = function() {
-
 	enemy.x += 2;
 	enemy.scale.x = 1;
-
-	// enemyPatrol = game.physics.arcade.collide(enemy)
 
 	game.physics.arcade.collide(enemy, platforms)
 
 	// If hadoken touches enemy, reduce health
   hadokenCollision = game.physics.arcade.collide(enemy, Hadokens);
+  // console.log(hadoken);
   if (hadokenCollision === true)
   {
-    enemy.enemyHealth--;
+    console.log("hadokenCollision");
+    enemyHealth--;
   }
 
 	if (enemy.health < 0)
