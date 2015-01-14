a7bf235ef70d01d85907492e2ea606703c6876d4
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-01-14 01:38:05 -0500

    remove audio information from game.js

--- public/js/game.js
+++ public/js/game.js
@@ -1,11 +1,10 @@
 var State = {};
-var music={};
+var music;
 
 State.preload = function(){
   game.load.tilemap('level1', 'assets/levels/level_one.json', null, Phaser.Tilemap.TILED_JSON);
   game.load.image('background', 'assets/imgs/background_level1.png');
   game.load.atlasJSONHash('ryuRun', 'assets/ryu/ryu.json', 'assets/ryu/ryu.png');
-  game.load.audio('theme', ['assets/audio/dp_pacmania_stage2.mp3']);
   Ryu.preload();
 
 };
@@ -25,18 +24,12 @@ State.create = function(){
   //resizes the game world
   //this.backgroundLayer.resizeWorld();
 
-  music = game.add.audio('theme',1,true);
-  music.override = true;
-  music.addMarker('map', 0, 1, true);
-  music.play('map');
-  // music.play();
 };
 State.update = function(){
   Ryu.update();
 };
 State.render = function() {
   Ryu.render();
-  // game.debug.soundInfo(music,0,100);
 };
 
 var game = game || new Phaser.Game(800,224,Phaser.AUTO,'ryuman',State);
